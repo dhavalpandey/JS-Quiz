2d250faa257644af4f6f2e715d0675c61b8a1841
Davdud <68287498+Davdud@users.noreply.github.com>
2020-11-09 19:40:31 +0000

    Add files via upload

---
+++ config.js
@@ -0,0 +1,33 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCzYCWd-z6E-_lqMKhN0Y07-rBpfwQD8xw",
+    authDomain: "gk-quiz-9765a.firebaseapp.com",
+    databaseURL: "https://gk-quiz-9765a.firebaseio.com",
+    projectId: "gk-quiz-9765a",
+    storageBucket: "gk-quiz-9765a.appspot.com",
+    messagingSenderId: "385707901761",
+    appId: "1:385707901761:web:5b6e08c7671516bca597c0"
+  };
+  firebase.initializeApp(firebaseConfig);
+
+let contactInfo = firebase.database().ref("contacts");
+
+document.getElementById('contactForm').addEventListener('submit', submitForm);
+
+function submitForm(e){
+    e.preventDefault();
+    var name = document.querySelector('.name').value;
+    var email = document.querySelector('.email').value;
+    var message = document.querySelector('.message').value;
+
+    saveContactInfo(name, email, message);
+}
+
+function saveContactInfo(name,email, message){
+    let newContactInfo = contactInfo.push();
+
+    newContactInfo.set({
+        username: name,
+        online_email: email,
+        feedback: message
+    });
+}
