7cf8f619f7d0f749b1836a044f8b5e6fce0a02b4
Davdud <68287498+Davdud@users.noreply.github.com>
2020-11-12 14:12:06 +0000

    Normal

--- app.js
+++ app.js
@@ -1,33 +1,132 @@
-var firebaseConfig = {
-    apiKey: "AIzaSyCzYCWd-z6E-_lqMKhN0Y07-rBpfwQD8xw",
-    authDomain: "gk-quiz-9765a.firebaseapp.com",
-    databaseURL: "https://gk-quiz-9765a.firebaseio.com",
-    projectId: "gk-quiz-9765a",
-    storageBucket: "gk-quiz-9765a.appspot.com",
-    messagingSenderId: "385707901761",
-    appId: "1:385707901761:web:5b6e08c7671516bca597c0"
-  };
-  firebase.initializeApp(firebaseConfig);
+var startButton = document.getElementById('start-btn');
+var nextButton = document.getElementById('next');
+var questionContainerElement = document.getElementById('question-container')
+var score = 0
+var attempted = 0
+var buttons = document.getElementById('btn')
 
-let contactInfo = firebase.database().ref("contacts");
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answers')
 
-document.getElementById('contactForm').addEventListener('submit', submitForm);
+let shuffledQuestions, currentQuestionIndex;
 
-function submitForm(e){
-    e.preventDefault();
-    var name = document.querySelector('.name').value;
-    var email = document.querySelector('.email').value;
-    var message = document.querySelector('.message').value;
+startButton.addEventListener('click', startGame)
+nextButton.addEventListener('click', () => {
+  currentQuestionIndex++
+  setNextQuestion()
+})
 
-    saveContactInfo(name, email, message);
+function startGame() {
+  startButton.classList.add('hide')
+  shuffledQuestions = questions.sort(() => Math.random() - .5)
+  currentQuestionIndex = 0
+  questionContainerElement.classList.remove('hide')
+  setNextQuestion()
 }
 
-function saveContactInfo(name,email, message){
-    let newContactInfo = contactInfo.push();
-
-    newContactInfo.set({
-        username: name,
-        online_email: email,
-        feedback: message
-    });
+function setNextQuestion() {
+  resetState()
+  showQuestion(shuffledQuestions[currentQuestionIndex])
 }
+
+function showQuestion(question) {
+  questionElement.innerText = question.question
+  question.answers.forEach(answer => {
+    const button = document.createElement('button')
+    button.innerText = answer.text
+    button.classList.add('btn')
+    if (answer.correct) {
+      button.dataset.correct = answer.correct
+    }
+    button.addEventListener('click', selectAnswer)
+    answerButtonsElement.appendChild(button)
+  })
+}
+
+function resetState() {
+  clearStatusClass(document.body)
+  nextButton.classList.add('hide')
+  while (answerButtonsElement.firstChild) {
+    answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+  }
+}
+
+function selectAnswer(e) {
+  const selectedButton = e.target
+  const correct = selectedButton.dataset.correct
+  setStatusClass(document.body, correct)
+  Array.from(answerButtonsElement.children).forEach(button => {
+    setStatusClass(button, button.dataset.correct)
+  })
+  if (shuffledQuestions.length > currentQuestionIndex + 1) {
+    nextButton.classList.remove('hide')
+  } else {
+    startButton.innerText = 'Restart'
+    startButton.classList.remove('hide')
+  }
+}
+
+function setStatusClass(element, correct) {
+  clearStatusClass(element)
+  if (correct) {
+    element.classList.add('correct')
+    attempted +=  1;
+   //console.log(attempted)
+  } else {
+    element.classList.add('wrong')
+    attempted += 1;
+  }
+}
+
+function clearStatusClass(element) {
+  element.classList.remove('correct')
+  element.classList.remove('wrong')
+}
+
+const questions = [
+  {
+    question: 'What is the capital of Spain?',
+    answers: [
+      { text: 'Barcelona', correct: false },
+      { text: 'Seville', correct: false },
+      { text: 'Madrid', correct: true },
+      { text: 'Valencia', correct: false }
+    ]
+  },
+  {
+    question: 'What is the largest country in the world?',
+    answers: [
+      { text: 'Russia', correct: true },
+      { text: 'USA', correct: false },
+      { text: 'India', correct: false },
+      { text: 'China', correct: false }
+    ]
+  },
+  {
+    question: 'What planet is closest to the sun?',
+    answers: [
+      { text: 'Venus', correct: false },
+      { text: 'Mercury', correct: true },
+      { text: 'Pluto', correct: false },
+      { text: 'Earth', correct: false }
+    ]
+  },
+  {
+    question: 'Who was the yongest footballer to score in the World Cup Final?',
+    answers: [
+      { text: 'Maradona', correct: false },
+      { text: 'Pele', correct: false },
+      { text: 'Ronaldo', correct: false },
+      { text: 'Mbappe', correct: true }
+    ]
+  },
+  {
+    question: 'Which country is most populated?',
+    answers: [
+      { text: 'China', correct: true },
+      { text: 'India', correct: false },
+      { text: 'Brazil', correct: false },
+      { text: 'USA', correct: false }
+    ]
+  }
+]
